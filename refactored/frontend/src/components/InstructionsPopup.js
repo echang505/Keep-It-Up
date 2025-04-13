3c9611e610a4b37a18aed381d665c104a125d5aa
Jonathan Xu <jonxu@umd.edu>
2025-04-13 08:03:45 -0400

    bug fixes

--- refactored/frontend/src/components/InstructionsPopup.js
+++ refactored/frontend/src/components/InstructionsPopup.js
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/InstructionsPopup.css';
-import clickSound from '../assets/sprites/mouse.mp3';
-import { useAudio } from '../context/AudioContext';
+import './InstructionsPopup.css';
 
 const InstructionsPopup = ({ onClose }) => {
-  const { playSound } = useAudio();
-  
-  const handleClose = () => {
-    playSound(clickSound);
-    onClose();
-  };
-  
   return (
     <div className="instructions-overlay">
       <div className="instructions-popup">
@@ -18,14 +9,14 @@ const InstructionsPopup = ({ onClose }) => {
         <div className="instructions-content">
           <p>Welcome to Keep It Up!</p>
           <ul>
-            <li>Use your thumb to keep falling objects in the air</li>
-            <li>Each successful bounce earns you points</li>
-            <li>If an object hits the ground, you lose</li>
+            <li>Click on the falling objects to keep them in the air</li>
+            <li>Each successful click earns you points</li>
+            <li>If an object hits the ground, you lose a life</li>
             <li>Try to get the highest score possible!</li>
           </ul>
           <p>Good luck!</p>
         </div>
-        <button className="close-button" onClick={handleClose}>
+        <button className="close-button" onClick={onClose}>
           Got it!
         </button>
       </div>
